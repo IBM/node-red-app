f6ae77302de22c3fa3061c4864adab3bd800ade0
jkingoliver <king@us.ibm.com>
2019-05-01 09:50:55 -0500

    initial

---
+++ bluemix-settings.js
@@ -0,0 +1,101 @@
+/**
+ * Copyright 2014, 2017, 2019 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+var path = require("path");
+var util = require("util");
+var fs = require("fs");
+var _ = require("lodash");
+var cfenv = require("cfenv");
+
+var appEnv = cfenv.getAppEnv();
+
+var userDir = path.join(__dirname, ".node-red");
+// Ensure userDir exists - something that is normally taken care of by
+// localfilesystem storage when running locally
+if (!fs.existsSync(userDir)) fs.mkdirSync(userDir);
+if (!fs.existsSync(path.join(userDir, "node_modules"))) fs.mkdirSync(path.join(userDir, "node_modules"));
+
+var settings = module.exports = {
+    uiPort: process.env.PORT || 1880,
+    mqttReconnectTime: 15000,
+    debugMaxLength: 1000,
+
+    //Flag for enabling Appmetrics dashboard (https://github.com/RuntimeTools/appmetrics-dash)
+    useAppmetrics: false,
+
+    userDir: userDir,
+
+    flowFile: "flows.json",
+
+    // Add the bluemix-specific nodes in
+    nodesDir: path.join(__dirname, "nodes"),
+
+    // Blacklist the non-bluemix friendly nodes
+    nodesExcludes: ['66-mongodb.js', '75-exec.js', '35-arduino.js', '36-rpi-gpio.js', '25-serial.js', '28-tail.js', '50-file.js', '31-tcpin.js', '32-udp.js', '23-watch.js'],
+
+    // Enable module reinstalls on start-up; this ensures modules installed
+    // post-deploy are restored after a restage
+    autoInstallModules: true,
+
+    // Move the admin UI
+    httpAdminRoot: '/red',
+
+    // Serve up the welcome page
+    httpStatic: path.join(__dirname, "public"),
+
+    functionGlobalContext: {},
+
+    // Configure the logging output
+    logging: {
+        // Only console logging is currently supported
+        console: {
+            // Level of logging to be recorded. Options are:
+            // fatal - only those errors which make the application unusable should be recorded
+            // error - record errors which are deemed fatal for a particular request + fatal errors
+            // warn - record problems which are non fatal + errors + fatal errors
+            // info - record information about the general running of the application + warn + error + fatal errors
+            // debug - record information which is more verbose than info + info + warn + error + fatal errors
+            // trace - record very detailed logging + debug + info + warn + error + fatal errors
+            // off - turn off all logging (doesn't affect metrics or audit)
+            level: "info",
+            // Whether or not to include metric events in the log output
+            metrics: false,
+            // Whether or not to include audit events in the log output
+            audit: true
+        }
+    }
+};
+
+// Look for the attached Cloudant instance to use for storage
+settings.couchAppname = appEnv.name;
+util.log("Setting couchAppname: " + settings.couchAppname)
+settings.couchDb = process.env.NODE_RED_STORAGE_DB_NAME || appEnv.name.replace(/[^a-z0-9_$()+/-]/g, "_");
+util.log("Setting CouchDb: " + settings.couchDb);
+
+var services = _.values(appEnv.getServices());
+var couchService = _.filter(services, { label: 'cloudantNoSQLDB' })[0];
+
+if (!couchService) {
+    util.log("Failed to find Cloudant service with label cloudantNoSQLDB ");
+    if (process.env.NODE_RED_STORAGE_NAME) {
+        util.log(" - using NODE_RED_STORAGE_NAME environment variable: " + process.env.NODE_RED_STORAGE_NAME);
+    }
+    // fall back to localfilesystem storage
+} else {
+    util.log("Using Cloudant service: " + couchService.name + " : " + settings.couchAppname);
+    settings.storageModule = require("./couchstorage");
+    settings.couchUrl = couchService.credentials.url;
+}
---
+++ couchstorage.js
@@ -0,0 +1,392 @@
+/**
+ * Copyright 2014, 2017, 2019 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+var nano = require('nano');
+var when = require('when');
+var util = require('util');
+var fs = require('fs');
+
+var settings;
+var appname;
+var flowDb = null;
+var currentFlowRev = null;
+var currentSettingsRev = null;
+var currentCredRev = null;
+
+var libraryCache = {};
+
+function prepopulateFlows(resolve) {
+    var key = appname+"/"+"flow";
+    flowDb.get(key,function(err,doc) {
+        if (err) {
+            var promises = [];
+            if (fs.existsSync(__dirname+"/defaults/flow.json")) {
+                try {
+                    var flow = fs.readFileSync(__dirname+"/defaults/flow.json","utf8");
+                    var flows = JSON.parse(flow);
+                    util.log("[couchstorage] Installing default flow");
+                    promises.push(couchstorage.saveFlows(flows));
+                } catch(err2) {
+                    util.log("[couchstorage] Failed to save default flow");
+                    util.log(err2);
+                }
+            } else {
+                util.log("[couchstorage] No default flow found");
+            }
+            if (fs.existsSync(__dirname+"/defaults/flow_cred.json")) {
+                try {
+                    var cred = fs.readFileSync(__dirname+"/defaults/flow_cred.json","utf8");
+                    var creds = JSON.parse(cred);
+                    util.log("[couchstorage] Installing default credentials");
+                    promises.push(couchstorage.saveCredentials(creds));
+                } catch(err2) {
+                    util.log("[couchstorage] Failed to save default credentials");
+                    util.log(err2);
+                }
+            } else {
+                util.log("[couchstorage] No default credentials found");
+            }
+            when.settle(promises).then(function() {
+                resolve();
+            });
+        } else {
+            // Flows already exist - leave them alone
+            resolve();
+        }
+    });
+}
+
+
+var couchstorage = {
+    init: function(_settings) {
+        settings = _settings;
+        var couchDb = nano(settings.couchUrl);
+        appname = settings.couchAppname || require('os').hostname();
+        var dbname = settings.couchDb||"nodered";
+        util.log("dbname: "+dbname  );
+
+        return when.promise(function(resolve,reject) {
+            couchDb.db.get(dbname,function(err,body) {
+                if (err) {
+                    couchDb.db.create(dbname,function(err,body) {
+                        if (err) {
+                            reject("Failed to create database: "+err);
+                        } else {
+                            flowDb = couchDb.use(dbname);
+                            flowDb.insert({
+                                views:{
+                                    flow_entries_by_app_and_type:{
+                                        map:function(doc) {
+                                            var p = doc._id.split("/");
+                                            if (p.length > 2 && p[2] == "flow") {
+                                                var meta = {path:p.slice(3).join("/")};
+                                                emit([p[0],p[2]],meta);
+                                            }
+                                        }
+                                    },
+                                    lib_entries_by_app_and_type:{
+                                        map:function(doc) {
+                                            var p = doc._id.split("/");
+                                            if (p.length > 2) {
+                                                if (p[2] != "flow") {
+                                                    var pathParts = p.slice(3,-1);
+                                                    for (var i=0;i<pathParts.length;i++) {
+                                                        emit([p[0],p[2],pathParts.slice(0,i).join("/")],{dir:pathParts.slice(i,i+1)[0]});
+                                                    }
+                                                    var meta = {};
+                                                    for (var key in doc.meta) {
+                                                        meta[key] = doc.meta[key];
+                                                    }
+                                                    meta.fn = p.slice(-1)[0];
+                                                    emit([p[0],p[2],pathParts.join("/")],meta);
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            },"_design/library",function(err,b) {
+                                if (err) {
+                                    reject("Failed to create view: "+err);
+                                } else {
+                                    prepopulateFlows(resolve);
+                                }
+                            });
+                        }
+                    });
+                } else {
+                    flowDb = couchDb.use(dbname);
+                    prepopulateFlows(resolve);
+                }
+            });
+        });
+    },
+
+    getFlows: function() {
+        var key = appname+"/"+"flow";
+        return when.promise(function(resolve,reject) {
+            flowDb.get(key,function(err,doc) {
+                if (err) {
+                    if (err.statusCode != 404) {
+                        reject(err.toString());
+                    } else {
+                        resolve([]);
+                    }
+                } else {
+                    currentFlowRev = doc._rev;
+                    resolve(doc.flow);
+                }
+            });
+        });
+    },
+
+    saveFlows: function(flows) {
+        var key = appname+"/"+"flow";
+        return when.promise(function(resolve,reject) {
+            var doc = {_id:key,flow:flows};
+            if (currentFlowRev) {
+                doc._rev = currentFlowRev;
+            }
+            flowDb.insert(doc,function(err,db) {
+                if (err) {
+                    reject(err.toString());
+                } else {
+                    currentFlowRev = db.rev;
+                    resolve();
+                }
+            });
+        });
+    },
+
+    getCredentials: function() {
+        var key = appname+"/"+"credential";
+        return when.promise(function(resolve,reject) {
+            flowDb.get(key,function(err,doc) {
+                if (err) {
+                    if (err.statusCode != 404) {
+                        reject(err.toString());
+                    } else {
+                        resolve({});
+                    }
+                } else {
+                    currentCredRev = doc._rev;
+                    resolve(doc.credentials);
+                }
+            });
+        });
+    },
+
+    saveCredentials: function(credentials) {
+        var key = appname+"/"+"credential";
+        return when.promise(function(resolve,reject) {
+            var doc = {_id:key,credentials:credentials};
+            if (currentCredRev) {
+                doc._rev = currentCredRev;
+            }
+            flowDb.insert(doc,function(err,db) {
+                if (err) {
+                    reject(err.toString());
+                } else {
+                    currentCredRev = db.rev;
+                    resolve();
+                }
+            });
+        });
+    },
+
+    getSettings: function() {
+        var key = appname+"/"+"settings";
+        return when.promise(function(resolve,reject) {
+            flowDb.get(key,function(err,doc) {
+                if (err) {
+                    if (err.statusCode != 404) {
+                        reject(err.toString());
+                    } else {
+                        resolve({});
+                    }
+                } else {
+                    currentSettingsRev = doc._rev;
+                    resolve(doc.settings);
+                }
+            });
+        });
+    },
+
+    saveSettings: function(settings) {
+        var key = appname+"/"+"settings";
+        return when.promise(function(resolve,reject) {
+            var doc = {_id:key,settings:settings};
+            if (currentSettingsRev) {
+                doc._rev = currentSettingsRev;
+            }
+            flowDb.insert(doc,function(err,db) {
+                if (err) {
+                    reject(err.toString());
+                } else {
+                    currentSettingsRev = db.rev;
+                    resolve();
+                }
+            });
+        });
+    },
+
+    getAllFlows: function() {
+        var key = [appname,"flow"];
+        return when.promise(function(resolve,reject) {
+            flowDb.view('library','flow_entries_by_app_and_type',{key:key}, function(e,data) {
+                if (e) {
+                    reject(e.toString());
+                } else {
+                    var result = {};
+                    for (var i=0;i<data.rows.length;i++) {
+                        var doc = data.rows[i];
+                        var path = doc.value.path;
+                        var parts = path.split("/");
+                        var ref = result;
+                        for (var j=0;j<parts.length-1;j++) {
+                            ref['d'] = ref['d']||{};
+                            ref['d'][parts[j]] = ref['d'][parts[j]]||{};
+                            ref = ref['d'][parts[j]];
+                        }
+                        ref['f'] = ref['f']||[];
+                        ref['f'].push(parts.slice(-1)[0]);
+                    }
+                    resolve(result);
+                }
+            });
+        });
+    },
+
+    getFlow: function(fn) {
+        if (fn.substr(0) != "/") {
+            fn = "/"+fn;
+        }
+        var key = appname+"/lib/flow"+fn;
+        return when.promise(function(resolve,reject) {
+            flowDb.get(key,function(err,data) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data.data);
+                }
+            });
+        });
+    },
+
+    saveFlow: function(fn,data) {
+        if (fn.substr(0) != "/") {
+            fn = "/"+fn;
+        }
+        var key = appname+"/lib/flow"+fn;
+        return when.promise(function(resolve,reject) {
+            var doc = {_id:key,data:data};
+            flowDb.get(key,function(err,d) {
+                if (d) {
+                    doc._rev = d._rev;
+                }
+                flowDb.insert(doc,function(err,d) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
+            });
+
+        });
+    },
+
+    getLibraryEntry: function(type,path) {
+        if (path != "" && path.substr(0,1) != "/") {
+            var key = appname+"/lib/"+type+"/"+path;
+        } else {
+            var key = appname+"/lib/"+type+path;
+        }
+        
+        if (libraryCache[key]) {
+            return when.resolve(libraryCache[key]);
+        }
+
+        return when.promise(function(resolve,reject) {
+            flowDb.get(key,function(err,doc) {
+                if (err) {
+                    if (path.substr(-1,1) == "/") {
+                        path = path.substr(0,path.length-1);
+                    }
+                    var qkey = [appname,type,path];
+                    flowDb.view('library','lib_entries_by_app_and_type',{key:qkey}, function(e,data) {
+                        if (e) {
+                            reject(e);
+                        } else {
+                            var dirs = [];
+                            var files = [];
+                            for (var i=0;i<data.rows.length;i++) {
+                                var row = data.rows[i];
+                                var value = row.value;
+
+                                if (value.dir) {
+                                    if (dirs.indexOf(value.dir) == -1) {
+                                        dirs.push(value.dir);
+                                    }
+                                } else {
+                                    files.push(value);
+                                }
+                            }
+                            libraryCache[key] = dirs.concat(files);
+                            resolve(libraryCache[key]);
+                        }
+                    });
+                } else {
+                    libraryCache[key] = doc.body;
+                    resolve(doc.body);
+                }
+            });
+        });
+    },
+    saveLibraryEntry: function(type,path,meta,body) {
+
+        var p = path.split("/");    // strip multiple slash   
+        p = p.filter(Boolean);
+        path = p.slice(0,p.length).join("/")
+                
+        if (path != "" && path.substr(0,1) != "/") {
+            path = "/"+path;
+        }
+        var key = appname+"/lib/"+type+path;
+        return when.promise(function(resolve,reject) {
+            var doc = {_id:key,meta:meta,body:body};
+            flowDb.get(key,function(err,d) {
+                if (d) {
+                    doc._rev = d._rev;
+                }
+                flowDb.insert(doc,function(err,d) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        var p = path.split("/");
+                        for (var i=0;i<p.length;i++) {
+                            delete libraryCache[appname+"/lib/"+type+(p.slice(0,i).join("/"))]
+                        }
+                        libraryCache[key] = body;
+                        resolve();
+                    }
+                });
+            });
+
+        });
+    }
+};
+
+module.exports = couchstorage;
