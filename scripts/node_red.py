628a7df90fc965572f894ba41b5a17c01e1731ef
Gabriel Valencia <gvalenc@us.ibm.com>
2020-02-26 17:05:31 -0600

    fix: selenium test fixes

--- scripts/node_red.py
+++ scripts/node_red.py
@@ -2,6 +2,10 @@ import os, time, sys, datetime, time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 date = datetime.datetime.now().strftime("%Y-%m-%d-%H%M:%S:%f")
 username = "devx-skit-governance"
@@ -16,15 +20,6 @@ options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(options=options)
-driver.get(os.environ["APP_URL"]); # Open a browser to the app's landing page
-
-is_setup_wizard = False
-h3_elems = driver.find_elements_by_xpath('//h3')
-for elem in h3_elems:
-    if elem.text == "Welcome to your new Node-RED instance on IBM Cloud":
-        is_setup_wizard = True
-        break
 
 def validate_landing_page():
     print("Checking if we've encountered the landing page")
@@ -56,26 +51,38 @@ def validate_landing_page():
     if not found_editor_link:
         sys.exit("Experience Test Failed: could not find landing page editor link")
 
-if is_setup_wizard:
-    print("Encountered initial setup wizard")
-    next_button = driver.find_element_by_xpath("//button[@id='btn-next']") # Locate the Next button
-    next_button.click()
+driver = webdriver.Chrome(options=options)
+try:
+    driver.get(os.environ["APP_URL"]); # Open a browser to the app's landing page
 
-    # set up as secure
-    secure_editor_radio_button = driver.find_element_by_xpath("//input[@id='secureOption-enabled']")
-    secure_editor_radio_button.click()
-    username_field = driver.find_element_by_xpath("//input[@input='secureOption-username']")
-    username_field.send_keys(username)
-    password_field = driver.find_element_by_xpath("//input[@input='secureOption-password']")
-    password_field.send_keys(password)
+    is_setup_wizard = False
+    h3_elems = driver.find_elements_by_xpath('//h3')
+    for elem in h3_elems:
+        if elem.text == "Welcome to your new Node-RED instance on IBM Cloud":
+            is_setup_wizard = True
+            break
+    if is_setup_wizard:
+        print("Encountered initial setup wizard")
+        next_button = driver.find_element_by_xpath("//button[@id='btn-next']") # Locate the Next button
+        next_button.click()
 
-    next_button.click() # go to next panel
-    next_button.click() # skip learning panel
+        # set up as secure
+        secure_editor_radio_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "btn-next")))
+        secure_editor_radio_button.click()
+        username_field = driver.find_element_by_xpath("//input[@input='secureOption-username']")
+        username_field.send_keys(username)
+        password_field = driver.find_element_by_xpath("//input[@input='secureOption-password']")
+        password_field.send_keys(password)
 
-    finish_button = driver.find_element_by_xpath("//button[@id='btn-finish']")
-    finish_button.click()
+        next_button.click() # go to next panel
+        next_button.click() # skip learning panel
+        
+        finish_button = driver.find_element_by_xpath("//button[@id='btn-finish']")
+        finish_button.click()
 
-    time.sleep(15)
-    validate_landing_page()
-else:
-    validate_landing_page()
+        time.sleep(15)
+        validate_landing_page()
+    else:
+        validate_landing_page()
+finally:
+    driver.quit()
