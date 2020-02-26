bb25514a5e9f188e230701a96c1d031344e1258d
Gabriel Valencia <gvalenc@us.ibm.com>
2020-02-26 16:11:21 -0600

    fix: add setup wizard tests

--- scripts/node_red.py
+++ scripts/node_red.py
@@ -1,4 +1,4 @@
-import os, time, sys, datetime
+import os, time, sys, datetime, time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -26,10 +26,7 @@ for elem in h3_elems:
         is_setup_wizard = True
         break
 
-if is_setup_wizard:
-    print("Encountered initial setup wizard")
-    next_button = driver.find_element_by_xpath("//button[@id='btn-next']") # Locate the Next button
-else:
+def validate_landing_page():
     print("Checking if we've encountered the landing page")
     found_node_red_heading = False
     h1_elems = driver.find_elements_by_xpath('//h1')
@@ -37,21 +34,21 @@ else:
         if elem.text == "Node-RED":
             found_node_red_heading = True
             break
-    
+
     found_node_red_subheading = False
     h2_elems = driver.find_elements_by_xpath('//h2')
     for elem in h2_elems:
         if elem.text == "Flow-based programming for the Internet of Things":
             found_node_red_subheading = True
             break
-    
+
     found_editor_link = False
     a_elems = driver.find_elements_by_xpath('//a')
     for elem in a_elems:
         if elem.text == "Go to your Node-RED flow editor":
             found_editor_link = True
             break
-    
+
     if not found_node_red_heading:
         sys.exit("Experience Test Failed: could not find landing page heading")
     if not found_node_red_subheading:
@@ -59,15 +56,26 @@ else:
     if not found_editor_link:
         sys.exit("Experience Test Failed: could not find landing page editor link")
 
-# item_box = driver.find_element_by_xpath("//input[@placeholder='item']") # Locate the text box
-# item_box.send_keys(date) # Enter some text
-# item_box.send_keys(Keys.ENTER) # Press ENTER
+if is_setup_wizard:
+    print("Encountered initial setup wizard")
+    next_button = driver.find_element_by_xpath("//button[@id='btn-next']") # Locate the Next button
+    next_button.click()
 
-# # Verify the action on the app's landing page
-# time.sleep(3)
-# output = driver.find_element_by_id('responseArea').text.splitlines()
-# print("The last entry in the 'responseArea' element is: {}".format(output[-1]))
-# if output[-1] == date:
-#     print("Experience Test Successful")
-# else:
-#     sys.exit("Experience Test Failed")
+    # set up as secure
+    secure_editor_radio_button = driver.find_element_by_xpath("//input[@id='secureOption-enabled']")
+    secure_editor_radio_button.click()
+    username_field = driver.find_element_by_xpath("//input[@input='secureOption-username']")
+    username_field.send_keys(username)
+    password_field = driver.find_element_by_xpath("//input[@input='secureOption-password']")
+    password_field.send_keys(password)
+
+    next_button.click() # go to next panel
+    next_button.click() # skip learning panel
+
+    finish_button = driver.find_element_by_xpath("//button[@id='btn-finish']")
+    finish_button.click()
+
+    time.sleep(15)
+    validate_landing_page()
+else:
+    validate_landing_page()
