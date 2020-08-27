4d0715e50ff19c02b9ea3b9e9d3b8edb40a217e2
Chuck Cox <chuckc@us.ibm.com>
2020-08-26 22:38:12 -0500

    test: removing window size arg

--- scripts/node_red.py
+++ scripts/node_red.py
@@ -51,7 +51,6 @@ options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--window-size=1200,600')
 driver = webdriver.Chrome(options=options)
 try:
     driver.get(os.environ["APP_URL"]);  # Open a browser to the app's landing page
