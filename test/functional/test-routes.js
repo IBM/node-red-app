503d61afb0705f619932a29610894b6beeb909ed
Chuck Cox <chuckc@us.ibm.com>
2020-10-28 13:21:28 -0500

    new: functional test

---
+++ test/functional/test-routes.js
@@ -0,0 +1,31 @@
+const chai = require('chai');
+const request = require('supertest');
+
+const expect = chai.expect;
+
+// example functional tests of routes
+describe('GET /', () => {
+  it('responds with homepage', () => {
+    return request(server)
+      .get('/')
+      .expect('Content-Type', 'text/html; charset=UTF-8')
+      .expect(200)
+      .then(response => {
+        expect(response.text).to.include(
+          'Welcome to your new Node-RED instance on IBM Cloud',
+        );
+      });
+  });
+});
+
+describe('GET /health', () => {
+  it('responds with json', () => {
+    return request(server)
+      .get('/health/')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect(200, {
+        status: 'UP',
+      });
+  });
+});
