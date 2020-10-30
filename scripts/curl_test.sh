d6b4e34c14752a7bfc91f0d1da73ca1fca197084
Chuck Cox <chuckc@us.ibm.com>
2020-10-30 09:18:18 -0500

    fix: url and add verbose

--- scripts/curl_test.sh
+++ scripts/curl_test.sh
@@ -2,6 +2,6 @@
 
 nohup bash -c "npm start 2>&1 &" && sleep 5
 echo "Checking '/' page..."
-curl -X GET "https://localhost:1880/"
+curl -v -X GET "http://localhost:1880/"
 echo "Checking '/health' page..."
-curl -X GET "https://localhost:1880/health"
+curl -v -X GET "http://localhost:1880/health"
