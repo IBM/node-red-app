14dc41266cb67b732065ec712d204e40e5b456f2
Chuck Cox <chuckc@us.ibm.com>
2020-10-30 09:43:06 -0500

    chore: improved output and logic

--- scripts/curl_test.sh
+++ scripts/curl_test.sh
@@ -4,9 +4,9 @@ echo "Starting Node-RED server..."
 nohup bash -c "npm start 2>&1 &" && sleep 5
 echo "Checking '/' page..."
 response1=$(curl --write-out '%{http_code}' --silent --output /dev/null "http://localhost:1880/")
-echo "$response1"
-if [[ $response1 = 200 ]]; then echo "Success"; else echo "Failed"; fi
+echo "HTTP Response: $response1"
+if [[ $response1 = 200 ]]; then echo "Success"; else echo "Failed" && exit 1; fi
 echo "Checking '/health' page..."
 response2=$(curl --write-out '%{http_code}' --silent --output /dev/null "http://localhost:1880/health")
-echo "$response2"
-if [[ $response2 = 200 ]]; then echo "Success"; else echo "Failed"; fi
+echo "HTTP Response: $response2"
+if [[ $response2 = 200 ]]; then echo "Success"; else echo "Failed" && exit 1; fi
