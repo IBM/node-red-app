58416996a298db17e943f9a9184608cbc1540f1e
Chuck Cox <chuckc@us.ibm.com>
2020-10-30 09:37:39 -0500

    test: curl response

--- scripts/curl_test.sh
+++ scripts/curl_test.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+echo "Starting Node-RED server..."
 nohup bash -c "npm start 2>&1 &" && sleep 5
 echo "Checking '/' page..."
-curl -v -X GET "http://localhost:1880/"
+response1=$(curl --write-out '%{http_code}' --silent --output /dev/null "http://localhost:1880/")
+echo "$response1"
+if [[ $response1 = 200 ]]; then echo "Success"; else echo "Failed"; fi
 echo "Checking '/health' page..."
-curl -v -X GET "http://localhost:1880/health"
+response2=$(curl --write-out '%{http_code}' --silent --output /dev/null "http://localhost:1880/health")
+echo "$response2"
+if [[ $response2 = 200 ]]; then echo "Success"; else echo "Failed"; fi
